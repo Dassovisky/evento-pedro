3eeded5cda7aa42cbc7f1616e898dddf654be0a5
dassovisky <dassaevtavares@gmail.com>
2018-04-26 20:46:54 -0300

    Initial commit

---
+++ App Mode Designer/www/xdk/xdk/js/index_user_scripts.js
@@ -0,0 +1,181 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+
+    
+        /* button  #btnZeros */
+    $(document).on("click", "#btnZeros", function(evt)
+    {
+       txtNumeros.placeholder = txtNumeros.placeholder + 0;
+         return false;
+    });
+    
+        /* button  #btnremover */
+    $(document).on("click", "#btnremover", function(evt)
+    {
+        /* your code goes here */
+        var textoantigo = txtNumeros.placeholder;
+        
+        txtNumeros.placeholder = txtNumeros.replace(textoantigo,'aaa');
+        
+         return false;
+    });
+    
+        /* button  #btnCadastro */
+    $(document).on("click", "#btnCadastro", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#CadastrarPet"); 
+         return false;
+    });
+    
+        /* button  #btnProcurar */
+    
+    
+        /* button  #btnComoFunciona */
+    $(document).on("click", "#btnComoFunciona", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ComoFunciona"); 
+         return false;
+    });
+    
+        /* button  #btnContato */
+    $(document).on("click", "#btnContato", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Contatenos"); 
+         return false;
+    });
+    
+        /* button  #btnEncontrei */
+    
+    
+        /* button  #btnEncontrei */
+    $(document).on("click", "#btnEncontrei", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#EncontreiPet"); 
+         return false;
+    });
+    
+        /* button  #btnEncontrei */
+    $(document).on("click", "#btnEncontrei", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#EncontreiPet"); 
+         return false;
+    });
+    
+        /* button  #btnEncontrei */
+    $(document).on("click", "#btnEncontrei", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ProcurarPet"); 
+         return false;
+    });
+    
+        /* button  #btnEncontrei */
+    $(document).on("click", "#btnEncontrei", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#EncontreiPet"); 
+         return false;
+    });
+    
+        /* button  #btnHome */
+    $(document).on("click", "#btnHome", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  #btnCadastrar */
+    $(document).on("click", "#btnCadastrar", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#CadastrarPet"); 
+         return false;
+    });
+    
+        /* button  #btnProcurar */
+    $(document).on("click", "#btnProcurar", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ProcurarPet"); 
+         return false;
+    });
+    
+        /* button  #btnComoFunciona */
+    $(document).on("click", "#btnComoFunciona", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ComoFunciona"); 
+         return false;
+    });
+    
+        /* button  #btnContatenos */
+    $(document).on("click", "#btnContatenos", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Contatenos"); 
+         return false;
+    });
+    
+        /* button  Button */
+    $(document).on("click", ".uib_w_71", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($("#navbar"));  
+         return false;
+    });
+    
+        /* button  #btncadastro */
+    $(document).on("click", "#btncadastro", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Cadastrarevento"); 
+         return false;
+    });
+    
+        /* button  #btnProcurar */
+    $(document).on("click", "#btnProcurar", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#achevento"); 
+         return false;
+    });
+    
+        /* button  #btnsobre */
+    $(document).on("click", "#btnsobre", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#sobrenos"); 
+         return false;
+    });
+    
+        /* button  Evento */
+    $(document).on("click", ".uib_w_74", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#evento"); 
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
